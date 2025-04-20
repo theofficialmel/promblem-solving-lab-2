ca4700f74377dfeab3ab1344b8570be75ac3284f
jamel <jamelmurray704@gmail.com>
2025-04-20 05:39:30 -0400

    Refactor array handling and enhance logging for favorite foods and party guests added and updated arrays.js part 4

--- part4-arrays.js
+++ part4-arrays.js
@@ -7,7 +7,31 @@
 //3. Add one more food using .push() and log the new array.
 
 let favoritefoods = ["pizza", "oatmeal", "meatloaf", "tacos","baked macaroni", "chocolate cake"];
+favoritefoods.push("sushi")
+console.log(favoritefoods)
 console.log(favoritefoods[0])
 console.log (favoritefoods[5])
 
-console.log(favoritefoods.push("oatmeal"))
+let partyguest = ["John", "Mary", "Paul", "Sarah", "Tom"];
+partyguest.shift("john")
+partyguest.push("jamel")
+console.log(partyguest)
+console.log(partyguest.length)
+
+
+let tasks = ["laundry", "groceries", "coding", "naps"];
+function printtasks(tasks) {
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(tasks[i]);
+    }
+}
+
+let numbers = [1, 2, 3, 4, 5];
+function listnumbers(numbers) {
+    for (let i = 5; i < numbers.length; i++) {}
+        console.log(numbers[i]);
+    }
+    
+
+
+
