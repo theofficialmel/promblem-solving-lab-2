35ce0c36ca41e7f3072a9ee2fa8d62ce50a00a39
jamel <jamelmurray704@gmail.com>
2025-04-24 07:34:21 -0400

    updated script.js and added to arrays.js

---
+++ script.js
@@ -0,0 +1,44 @@
+let numb = [0,1,2,3,4.5,6,7,8,9,10]; 
+for (let i = 0; i < numb.length; i++){
+console.log(numb[i]);
+}
+
+ 
+for(let i = 2; i <= 10; i+=2){
+    console.log(i);
+} 
+
+
+let animals = ["dog", "cat", "rabbit", "parrot"];
+for (let i =0; i< animals.length; i++){
+    console.log('i like dogs') + animals[0];
+    console.log('i like cats') + animals[1];
+    console.log('i like rabbit') + animals[2];
+    console.log('i like parrot') + animals[3];
+}
+
+let colors = ["red", "green", "blue", "yellow"];
+ {
+    colors.reverse();
+    console.log(colors); 
+    
+}
+
+let person = ["jamel"]
+
+let hobbies = ["gaming","basketball", "tv"]
+
+let age = ["25"]
+
+let favoritecolor = ["blue"]
+        console.log("my name is " + person + " and my age is " + age + " and my favorite color is " + favoritecolor + "my favorite hobbies are" + hobbies);
+
+
+
+
+
+
+
+
+
+
